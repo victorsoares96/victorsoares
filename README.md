58f7d150df17a5edddaa52446cf7ca02d7238177
Victor Soares <victorsoares@Victors-iMac-Pro.local>
2020-06-06 17:39:33 -0300

    version 1.0

---
+++ README.md
@@ -0,0 +1,26 @@
+<h2 align="center">
+  Cartão Pessoal em NPX!
+</h2>
+
+<h1 align="center">
+  <a href="https://github.com/victorsoares96/victorsoares96">
+    <img alt="npx_image" src="https://user-images.githubusercontent.com/19741953/83953873-8f005080-a81a-11ea-9285-42dc441e99c9.png" width="300px" />
+  </a>
+</h1>
+
+<p align="center">
+  <a href="https://github.com/victorsoares96">
+    <img alt="Made by Victor Soares" src="https://img.shields.io/badge/made%20by-victorsoares96-blue">
+    <img alt="shield" src="https://img.shields.io/david/victorsoares96/victorsoares96">
+    <img alt="size" src="https://img.shields.io/github/languages/code-size/victorsoares96/victorsoares96">
+  </a>
+</p>
+
+## About
+
+Este é um package npm que simula um cartão pessoal. Este exemplo foi baseado no cartão pessoal do repositório da **[glaucia_lemos86](https://www.npmjs.com/package/glaucia_lemos86)**.
+
+
+## Quer ver funcionar?
+
+- `npx victorsoares96`
---
+++ bin/card.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+// Usado para dizer ao Node.js que se trata de uma ferramenta do CLI
+
+// Declarando os módulos:
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Definindo as opções para o ‘boxen’:
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Inclusão do Text e mais as definições do ‘chalk’:
+const data = {
+  name: chalk.white('                Victor Soares /'),
+  handle: chalk.cyan('victorsoares96'),
+  work: chalk.white('Mobile and Web Front End Developer'),
+  twitter: chalk.cyan('https://twitter.com/VictorSoares_96'),
+  github: chalk.cyan('https://github.com/victorsoares96'),
+  linkedin: chalk.cyan('https://www.linkedin.com/in/victor-soares96'),
+  instagram: chalk.cyan('https://www.instagram.com/vitu.soares'),
+  npx: chalk.white('npx victorsoares96'),
+  
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelInstagram: chalk.white.bold(' Instagram:'),
+  labelCard: chalk.white.bold('      Card:'),
+  labelFinal: chalk.yellow.bold(`
+    console.log('${chalk.white('Nunca desista dos seus sonhos!')}')
+  `)
+}
+
+// Aqui será a saída do nosso Cartão Pessoal em NPX:
+const newline = '\n'
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const instagram = `${data.labelInstagram}  ${data.instagram}`
+const carding = `${data.labelCard}  ${data.npx}`
+const final = `${data.labelFinal}`
+
+// Aqui devemos colocar toda a nossa saída numa única variável para que possamos usar 
+// o ‘boxen de maneira efetiva: 
+const output = (
+  heading + newline + newline + 
+  working + newline + 
+  twittering + newline + 
+  githubing + newline + 
+  linkedining + newline + 
+  instagram + newline + 
+  newline + carding +
+  newline + final
+);
+
+console.log(chalk.green(boxen(output, options)));
